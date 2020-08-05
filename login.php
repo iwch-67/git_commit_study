2c379335bdcbaafb77a023c308b18fade5342083
iwch-67 <gekiuma4@gmail.com>
2020-08-05 19:03:58 +0900

    [Add][ログイン画面] ログイン機能の実装
    名前とメールアドレスのフォームを作成し、これらを用いてログインします。

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>ログイン</title>
+</head>
+<body>
+<h1>ログインページ</h1>
+<form action="" method="post">
+<label for="name">名前</label>
+<input type="text" name="name" id="name"><br>
+<label for="email">メールアドレス</label>
+<input type="email" name="email" id="email"><br>
+<input type="submit" value="ログイン">
+</form>
+</body>
+</html>
